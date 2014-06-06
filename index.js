a15fdd8159d283b1ad86099e4134802195166e57
yoshuawuyts <i@yoshuawuyts.com>
2014-06-06 15:09:51 +0200

    Simplified syntax

--- index.js
+++ index.js
@@ -23,52 +23,37 @@ function customs (type, target) {
   switch (type) {
 
     case 'array':
-      if ('[object Array]' == Object.prototype.toString.call(target)){
-        return true;
-      }
+      if ('[object Array]' == Object.prototype.toString.call(target)) return true;
       return false;
 
     case 'string':
-      if ('string' == typeof(target)) {
-        return true;
-      }
+      if ('string' == typeof target) return true;
       return false;
 
     case 'number':
-      if ('number' == typeof(target)) {
-        return true
-      };
+      if ('number' == typeof target) return true;
       return false;
 
     case 'boolean':
-      if ('boolean' == typeof(target)) {
-        return true;
-      }
+      if ('boolean' == typeof target) return true;
       return false;
 
     case 'object':
-      if ('object' == typeof(target)) {
-        return true;
-      }
+      if ('object' == typeof target) return true;
       return false;
 
     case 'email':
       var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if (regex.test(target)) {
-        return true;
-      }
+      if (regex.test(target)) return true;
       return false;
 
     case 'function':
-      console.log(typeof target)
       if('function' == typeof target) return true;
       return false;
 
     default:
       // check and evaluate regex
-      if (type instanceof RegExp && type.test(target)) {
-        return true;
-      }
+      if (type instanceof RegExp && type.test(target)) return true;
       return false;
-  }
-}
+  };
+};
