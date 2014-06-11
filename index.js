6c6b8d652045d736ad684c83f24a3a05d08e057d
yoshuawuyts <i@yoshuawuyts.com>
2014-06-11 16:01:48 +0200

    Refactor

--- index.js
+++ index.js
@@ -23,37 +23,29 @@ function customs (type, target) {
   switch (type) {
 
     case 'array':
-      if ('[object Array]' == Object.prototype.toString.call(target)) return true;
-      return false;
+      return '[object Array]' == Object.prototype.toString.call(target);
 
     case 'string':
-      if ('string' == typeof target) return true;
-      return false;
+      return 'string' == typeof target;
 
     case 'number':
-      if ('number' == typeof target) return true;
-      return false;
+      return 'number' == typeof target;
 
     case 'boolean':
-      if ('boolean' == typeof target) return true;
-      return false;
+      return 'boolean' == typeof target;
 
     case 'object':
-      if ('object' == typeof target) return true;
-      return false;
+      return 'object' == typeof target;
+
+    case 'function':
+      return 'function' == typeof target;
 
     case 'email':
       var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if (regex.test(target)) return true;
-      return false;
-
-    case 'function':
-      if('function' == typeof target) return true;
-      return false;
+      return regex.test(target);
 
     default:
-      // check and evaluate regex
-      if (type instanceof RegExp && type.test(target)) return true;
-      return false;
+      if (type.constructor != RegExp) return false;
+      return type.test(target);
   };
 };
